4660020b0ef631724fb24b75f2e5f7ac89ab3d22
nd0ut <nd0ut.me@gmail.com>
2013-11-25 14:31:13 +0400

    adapt to posada xml format

--- prepare_data.py
+++ prepare_data.py
@@ -2,10 +2,16 @@ import pdb
 import psycopg2
 import urllib
 from xml.dom.minidom import *
-
 from pydap.client import open_url
 
 import parsers
+
+
+def addPydapToUrl(url):
+  return insert(url, 'pydap/', len(root_path))
+
+def insert(original, new, pos):
+  return original[:pos] + new + original[pos:]
 
 # Postgres config
 POSTGRES_USER = "postgres"
@@ -28,25 +34,26 @@ folders = xml.getElementsByTagName('catalogRef')
 
 for folder in folders:
   if folder.attributes['name'].value != 'weeks':
-    year_url = root_path + folder.attributes['ID'].value
+    year_url = addPydapToUrl(folder.attributes['xlink:href'].value)
     year_name = folder.attributes['name'].value
 
     print 'Processing %s year' % year_name[1:]
 
     # parse each month
-    xml = parse(urllib.urlopen(year_url + 'catalog.xml'))
+    xml = parse(urllib.urlopen(year_url))
     month_folders = xml.getElementsByTagName('catalogRef')
 
     for month_folder in month_folders:
-      month_url = root_path + month_folder.attributes['ID'].value
+      month_url = addPydapToUrl(month_folder.attributes['xlink:href'].value)
 
       # parse each file
-      xml = parse(urllib.urlopen(month_url + 'catalog.xml'))
+      xml = parse(urllib.urlopen(month_url))
       files = xml.getElementsByTagName('dataset')[0].getElementsByTagName('dataset')
 
-      for file in files:
-        file_url = root_path + file.attributes['ID'].value
-        file_name = file.attributes['name'].value
+      for data_file in files:
+        file_url = root_path + 'pydap/' + data_file.attributes['urlPath'].value
+        file_name = data_file.attributes['name'].value
+
         # add data to database
         dataset = open_url(file_url)
         parsers.wind.parse(con, dataset)
